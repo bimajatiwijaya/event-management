21f9af1f7caa40419a900971b19e08d795fcd256
bima <bima@portcities.net>
2017-12-03 21:04:31 +0700

    [ADD] Initial event android project * Spesial thank you for sharing codes and template (https://github.com/sourcey/materiallogindemo) * Adjust method locally.

---
+++ src/main/java/com/example/root/event/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.root.event;
+
+/**
+ * Created by root on 02/12/17.
+ */
+
+import android.app.Application;
+
+public class MainActivity extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Required initialization logic here!
+    }
+}
